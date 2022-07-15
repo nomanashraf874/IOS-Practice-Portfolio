f2b4c3eca70ec76a72b439d4949935db3d598b35
Noman Ashraf <73111863+nomanashraf874@users.noreply.github.com>
2022-07-15 16:02:35 -0400

    Add files via upload

---
+++ BMI-Calculator/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Noman Ashraf on 7/11/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+}
